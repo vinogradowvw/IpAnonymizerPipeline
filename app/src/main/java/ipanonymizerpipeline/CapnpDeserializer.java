fe604582f3f44a7afd8eb5d7f0b91c21d2ea7b6f
Grigorii Vinogradov <143388794+vinogradowvw@users.noreply.github.com>
2024-03-16 16:19:45 +0100

    added comments.java

--- app/src/main/java/ipanonymizerpipeline/CapnpDeserializer.java
+++ app/src/main/java/ipanonymizerpipeline/CapnpDeserializer.java
@@ -6,6 +6,10 @@ import org.capnproto.ArrayInputStream;
 import org.capnproto.MessageReader;
 import org.capnproto.Serialize;
 
+/*
+ * Getting the deserializer object to get the data in correct data types
+ */
+
 public class CapnpDeserializer {
     
     public static HtmlLog.HttpLogRecord.Reader getDeserializer (ByteBuffer buf) throws java.io.IOException {
